cb20c3734fd2e670dec0e4318d42c166c9d0e194
amcamd <andrew.chapman@amd.com>
2017-08-10 14:24:04 -0500

    make h2axpy more like reference axpy

--- Examples/h2axpy/h2axpy_dx4.cpp
+++ Examples/h2axpy/h2axpy_dx4.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<hip/hip_runtime.h>
 #include<hip/hip_runtime_api.h>
+#include "rocblas.h"
 
 typedef __fp16 half8 __attribute__((ext_vector_type(8)));
 typedef __fp16 half2 __attribute__((ext_vector_type(2)));
@@ -9,6 +10,7 @@ extern "C" half2 __v_pk_fma_f16(half2, half2, half2) __asm("llvm.fma.v2f16");
 
 #define LEN 1024*1024
 #define SIZE LEN*sizeof(half8)
+#define NB_X 128
 
 #define CHECK_HIP_ERROR(error) \
 if (error != hipSuccess) { \
@@ -16,6 +18,18 @@ if (error != hipSuccess) { \
     exit(EXIT_FAILURE);\
 }
 
+#define CHECK_ROCBLAS_ERROR(error) \
+if (error != rocblas_status_success) { \
+    fprintf(stderr, "rocBLAS error: "); \
+    if(error == rocblas_status_invalid_handle)fprintf(stderr, "rocblas_status_invalid_handle"); \
+    if(error == rocblas_status_not_implemented)fprintf(stderr, " rocblas_status_not_implemented"); \
+    if(error == rocblas_status_invalid_pointer)fprintf(stderr, "rocblas_status_invalid_pointer"); \
+    if(error == rocblas_status_invalid_size)fprintf(stderr, "rocblas_status_invalid_size"); \
+    if(error == rocblas_status_memory_error)fprintf(stderr, "rocblas_status_memory_error"); \
+    if(error == rocblas_status_internal_error)fprintf(stderr, "rocblas_status_internal_error"); \
+    fprintf(stderr, "\n"); \
+    exit(EXIT_FAILURE); \
+}
 
 __global__ void h2Axpy(int n, half2 alpha, half8 *X, half8 *Y) {
     int tx = hipThreadIdx_x + hipBlockIdx_x * 1024;
@@ -56,29 +70,69 @@ __global__ void h2Axpy(int n, half2 alpha, half8 *X, half8 *Y) {
     Y[tx][7] = z3.y;
 }
 
+
+extern "C"
+rocblas_status
+rocblas_haxpy(rocblas_handle handle,
+    rocblas_int n,
+    const __fp16 *alpha,
+    const __fp16 *x, rocblas_int incx,
+          __fp16 *y,  rocblas_int incy)
+{
+    if (nullptr == alpha)
+        return rocblas_status_invalid_pointer;
+    else if (nullptr == x)
+        return rocblas_status_invalid_pointer;
+    else if (nullptr == y)
+        return rocblas_status_invalid_pointer;
+    else if(nullptr == handle)
+        return rocblas_status_invalid_handle;
+
+    if(1 != incx || 1 != incy) return rocblas_status_not_implemented;
+    if(0 != n % 8)             return rocblas_status_not_implemented;
+
+    /*
+     * Quick return if possible. Not Argument error
+     */
+    if ( n <= 0 )
+        return rocblas_status_success;
+
+    half2 half2_alpha;
+    half2_alpha.x = *alpha;
+    half2_alpha.y = *alpha;
+
+    hipLaunchKernelGGL(h2Axpy, dim3((n/8)/1024,1,1), dim3(1024,1,1), 0, 0, 
+            n, half2_alpha, (half8*)x, (half8*)y);
+
+    return rocblas_status_success;
+}
+
+
 int main() {
-    int n = LEN * 8;
-    std::vector<__fp16>X(n), Y(n), Ycopy(n);
+    int                 n = LEN * 8, incx = 1, incy = 1;
+    __fp16              alpha = 3.0, *Xd, *Yd;
+    std::vector<__fp16> X(n), Y(n), Ycopy(n);
+
     for(int i = 0; i < Y.size(); i++)
     { 
-//      Y[i] = __fp16(i%2048);    ERROR at 1961
         Y[i] = __fp16(i%128); 
         X[i] = __fp16((i+4)%64);
     }
+
     Ycopy = Y;
-    hipSetDevice(1);
-    __fp16 *Xd, *Yd;
     CHECK_HIP_ERROR(hipMalloc(&Xd, SIZE));
     CHECK_HIP_ERROR(hipMalloc(&Yd, SIZE));
     CHECK_HIP_ERROR(hipMemcpy(Xd, X.data(), SIZE, hipMemcpyHostToDevice));
     CHECK_HIP_ERROR(hipMemcpy(Yd, Y.data(), SIZE, hipMemcpyHostToDevice));
-    half2 alpha;
-    alpha.x = __fp16(3.0);
-    alpha.y = __fp16(2.0);
+
+    rocblas_handle handle;
+    CHECK_ROCBLAS_ERROR(rocblas_create_handle(&handle));
+
+    hipSetDevice(1);
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    hipLaunchKernelGGL(h2Axpy, dim3((n/8)/1024,1,1), dim3(1024,1,1), 0, 0, n, alpha, (half8*)Xd, (half8*)Yd);
+    CHECK_ROCBLAS_ERROR(rocblas_haxpy( handle, n, &alpha, Xd, incx, Yd, incy));
 
     CHECK_HIP_ERROR(hipDeviceSynchronize());
 
@@ -88,7 +142,7 @@ int main() {
     std::cout<<"X, Ycopy, Y    (EVEN)"<<std::endl;
     for (int i = 0; i < 20; i+=2)
     {
-        if(float(Y[i] == float(alpha.x) * float(X[i]) + float(Ycopy[i]))) 
+        if(float(Y[i] == float(alpha) * float(X[i]) + float(Ycopy[i]))) 
         {
             std::cout<<(float)X[i]<<", "<<(float)Ycopy[i]<<", "<<(float)Y[i]<<std::endl;
         }
@@ -100,7 +154,7 @@ int main() {
     std::cout<<"X, Ycopy, Y    (ODD)"<<std::endl;
     for (int i = 1; i < 20; i+=2)
     {
-        if(float(Y[i] == float(alpha.y) * float(X[i]) + float(Ycopy[i]))) 
+        if(float(Y[i] == float(alpha) * float(X[i]) + float(Ycopy[i]))) 
         {
             std::cout<<(float)X[i]<<", "<<(float)Ycopy[i]<<", "<<(float)Y[i]<<std::endl;
         }
@@ -113,9 +167,9 @@ int main() {
     std::cout<<"Y.size() = "<<Y.size()<<std::endl;
     int even_error = 0;
     for(int i=0; i<Y.size(); i+=2) {
-        float out = float(alpha.x) * float(X[i]) + float(Ycopy[i]);
+        float out = float(alpha) * float(X[i]) + float(Ycopy[i]);
         if(float(Y[i]) != out) {
-            if(even_error < 100) 
+            if(even_error < 10) 
                 std::cerr<<"Bad even output: "<<float(Y[i])<<" at: "<<i<<" Expected: "<<out<<std::endl;
             even_error++;
         }
@@ -123,9 +177,9 @@ int main() {
     if(0 == even_error)std::cout<<"---All even pass---\n";
     int odd_error = 0;
     for(int i=1; i<Y.size(); i+=2) {
-        float out = float(alpha.y) * float(X[i]) + float(Ycopy[i]);
+        float out = float(alpha) * float(X[i]) + float(Ycopy[i]);
         if(float(Y[i]) != out) {
-            if(odd_error < 100) 
+            if(odd_error < 10) 
                 std::cerr<<"Bad odd output: "<<float(Y[i])<<" at: "<<i<<" Expected: "<<out<<std::endl;
             odd_error++;
         }
