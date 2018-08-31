e602e5b478dccf62ec35c2e0d97a18153a7f39ad
amcamd <andrew.chapman@amd.com>
2018-08-31 18:09:47 -0500

    simple fdot2 code for hpa on vega20

--- Examples/hpa_hgemv/hpa_hgemv.cpp
+++ Examples/hpa_hgemv/hpa_hgemv.cpp
@@ -3,13 +3,10 @@
 #include<hip/hip_runtime_api.h>
 #include "rocblas.h"
 
-typedef _Float16 half8 __attribute__((ext_vector_type(8)));
-typedef _Float16 half2 __attribute__((ext_vector_type(2)));
+typedef __fp16 half8 __attribute__((__vector_size__(8*sizeof(__fp16))));
+typedef __fp16 half2 __attribute__((__vector_size__(2*sizeof(__fp16))));
 
-extern "C" __device__ half2 __v_pk_fma_f16(half2, half2, half2) __asm("llvm.fma.v2f16");
- 
-extern "C" __device__ float __builtin_amdgcn_fdot2(half2, half2, float) __asm("v_dot2_f32_f16");
-//extern "C" __device__ float __builtin_amdgcn_fdot2(half2, half2, float);
+extern "C" __device__ float __builtin_amdgcn_fdot2(half2, half2, float);
 
 #define NB 128
 #define NB_X 256
@@ -34,64 +31,50 @@ if (error != rocblas_status_success) { \
 }
 
 
-
 __global__
 void gemv_kernel_host_scalar(hipLaunchParm   lp,
-                        rocblas_int     n1, 
-                        rocblas_int     n2,
-                        const _Float16  *a,
-                        rocblas_int     lda,
-                        const _Float16  *x,
-                        _Float16        *y)
+                        rocblas_int          n1, 
+                        rocblas_int          n2_div_8,
+                        const half8          *aa,
+                        rocblas_int          lda_div_8,
+                        const half8          *xx,
+                        __fp16               *yy,
+                        float                alpha,
+                        float                beta)
 {
-    float alpha = 1.0;
-    float beta = 1.0;
     int tid = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
     if(tid < n1)
     {
-//      for(int i = 0; i < n2; i++)
-//      {
-//          y[tid] += a[tid + i*lda] * x[i];
-//      }
-
-        half8 *aa = (half8 *)(&(a[tid*lda]));
-        half8 *xx = (half8 *)(x);
-
-        float c = 10.0;
-        float d = 0.0;
-        for(int i8 = 0; i8 < n2; i8 += 8)
+        float c = 0.0;
+        for(int i8 = 0; i8 < n2_div_8; i8 += 1)
         {
-            half8 *xx = (half8 *)(&(xx[i8]));
-            half8 *aa = (half8 *)(&(aa[i8]));
-
             half2 a0, a1, a2, a3;
             half2 x0, x1, x2, x3;
 
-            a0[0] = 1.0; //  aa[i8][0];
-            a0[1] = 1.0; //  aa[i8][1];
-            a1[0] = 1.0; //  aa[i8][2];
-            a1[1] = 1.0; //  aa[i8][3];
-            a2[0] = 1.0; //  aa[i8][4];
-            a2[1] = 1.0; //  aa[i8][5];
-            a3[0] = 1.0; //  aa[i8][6];
-            a3[1] = 1.0; //  aa[i8][7];
+            a0[0] = aa[i8 + tid*lda_div_8][0];
+            a0[1] = aa[i8 + tid*lda_div_8][1];
+            a1[0] = aa[i8 + tid*lda_div_8][2];
+            a1[1] = aa[i8 + tid*lda_div_8][3];
+            a2[0] = aa[i8 + tid*lda_div_8][4];
+            a2[1] = aa[i8 + tid*lda_div_8][5];
+            a3[0] = aa[i8 + tid*lda_div_8][6];
+            a3[1] = aa[i8 + tid*lda_div_8][7];
 
-            x0[0] = 1.0; //  xx[i8][0];
-            x0[1] = 1.0; //  xx[i8][1];
-            x1[0] = 1.0; //  xx[i8][2];
-            x1[1] = 1.0; //  xx[i8][3];
-            x2[0] = 1.0; //  xx[i8][4];
-            x2[1] = 1.0; //  xx[i8][5];
-            x3[0] = 1.0; //  xx[i8][6];
-            x3[1] = 1.0; //  xx[i8][7];
+            x0[0] = xx[i8][0];
+            x0[1] = xx[i8][1];
+            x1[0] = xx[i8][2];
+            x1[1] = xx[i8][3];
+            x2[0] = xx[i8][4];
+            x2[1] = xx[i8][5];
+            x3[0] = xx[i8][6];
+            x3[1] = xx[i8][7];
 
-//          d = my_fdot2(a0, x0, c);
-            d = __builtin_amdgcn_fdot2(a0, x0, c);
-//          c = __builtin_amdgcn_fdot2(a1, x1, c, true);
-//          c = __builtin_amdgcn_fdot2(a2, x2, c, true);
-//          c = __builtin_amdgcn_fdot2(a3, x3, c, true);
+            c = __builtin_amdgcn_fdot2(a0, x0, c);
+            c = __builtin_amdgcn_fdot2(a1, x1, c);
+            c = __builtin_amdgcn_fdot2(a2, x2, c);
+            c = __builtin_amdgcn_fdot2(a3, x3, c);
         }
-        y[tid] = static_cast<_Float16>(alpha * c) + static_cast<_Float16>(beta * y[tid]);
+        yy[tid] = static_cast<__fp16>(alpha * c) + static_cast<__fp16>(beta * yy[tid]);
     }
 }
 
@@ -100,10 +83,12 @@ rocblas_status
 test_hgemv(rocblas_handle handle,
     rocblas_int n1, 
     rocblas_int n2,
-    const _Float16 *a, 
+    const __fp16 *a, 
     rocblas_int lda,
-    const _Float16 *x,
-          _Float16 *y)
+    const __fp16 *x,
+          __fp16 *y,
+    float   alpha,
+    float   beta)
 {
     if (nullptr == a)
         return rocblas_status_invalid_pointer;
@@ -125,8 +110,15 @@ test_hgemv(rocblas_handle handle,
     dim3 threads(NB_X, 1, 1);
 
     hipLaunchKernel(HIP_KERNEL_NAME(gemv_kernel_host_scalar),
-            dim3(blocks), dim3(threads), 0, 0,
-            n1, n2, a, lda, x, y);
+                                    dim3(blocks), dim3(threads), 0, 0,
+                                    n1, 
+                                    n2/8, 
+                                    reinterpret_cast<const half8*>(a), 
+                                    lda/8, 
+                                    reinterpret_cast<const half8*>(x), 
+                                    y, 
+                                    alpha, 
+                                    beta);
 
     return rocblas_status_success;
 }
@@ -134,9 +126,6 @@ test_hgemv(rocblas_handle handle,
 int parse_args(int argc, char *argv[], int &n, int &incx, int &incy)
 {
     // default values
-    n = 8;
-    incx = 1;
-    incy = 1;
 
     while (argc > 1)
     {
@@ -177,28 +166,30 @@ void usage(char *argv[])
 
 int main(int argc, char *argv[]) 
 {
-    int n=8, incx=1, incy=1;
-    _Float16 alpha = 1.0;
+    int n=16, incx=1, incy=1;
+    float alpha = 1.0;
+    float beta  = 2.0;
     if (parse_args(argc, argv, n, incx, incy))
     {
         usage(argv);
         return -1;
     }
-    std::cout << "n, incx, incy = " << n << ", " << incx << ", " << incy << std::endl;
+    std::cout << "n, incx, incy, alpha, beta = " << n << ", " << incx << ", " << incy << ", " 
+              << alpha << ", " << beta << ", " << std::endl;
         
     int n1 = n;
     int n2 = n;
     int lda = n1;
-    std::vector<_Float16> A(lda*n2), X(n2), Y(n2), Y_gold(n2);
-    int sizeX = X.size() * sizeof(_Float16);
-    int sizeY = Y.size() * sizeof(_Float16);
-    int sizeA = A.size() * sizeof(_Float16);
+    std::vector<__fp16> A(lda*n2), X(n2), Y(n2), Y_gold(n2);
+    int sizeX = X.size() * sizeof(__fp16);
+    int sizeY = Y.size() * sizeof(__fp16);
+    int sizeA = A.size() * sizeof(__fp16);
 
     for(int i1 = 0; i1 < n; i1++)
     {
         for(int i2 = 0; i2 < n; i2++)
         {
-            A[i1+i2*lda] = static_cast<_Float16>(1);
+            A[i1+i2*lda] = static_cast<__fp16>(rand()%10);
         }
     }
     std::cout << "-----------A[" << n1 << "," << n2 << "]-------------------" << std::endl;
@@ -210,31 +201,30 @@ int main(int argc, char *argv[])
 
     for(int i = 0; i < X.size(); i++)
     { 
-        X[i] = static_cast<_Float16>(1);
+        X[i] = static_cast<__fp16>(rand()%10);
     }
     for(int i = 0; i < Y.size(); i++)
     { 
-        Y[i] = static_cast<_Float16>(1); 
+        Y[i] = static_cast<__fp16>(rand()%10); 
     }
 
     Y_gold = Y;
     for(int i1 = 0; i1 < n; i1++)
     {
-        _Float16 t = 0.0;
+        float t = 0.0;
         for(int i2 = 0; i2 < n; i2++)
         {
-            t += A[i1+i2*lda] * X[i2];
+            t += static_cast<float>(A[i1*lda+i2]) * static_cast<float>(X[i2]);
         }
-        Y_gold[i1] += alpha * t;
+        Y_gold[i1] = static_cast<__fp16>(beta * static_cast<float>(Y_gold[i1]) + alpha * t);
     }
 
     std::cout << "--------------x,y,y_gold---------------------" << std::endl;
     for(int i = 0; i < X.size(); i++){std::cout << static_cast<float>(X[i]) << ",";}; std::cout << std::endl;
     for(int i = 0; i < Y.size(); i++){std::cout << static_cast<float>(Y[i]) << ",";}; std::cout << std::endl;
     for(int i = 0; i < Y_gold.size(); i++){std::cout << static_cast<float>(Y_gold[i]) << ",";}; std::cout << std::endl;
-//  std::cout << "---------------------------------------------" << std::endl;
 
-    _Float16 *Ad, *Xd, *Yd;
+    __fp16 *Ad, *Xd, *Yd;
     CHECK_HIP_ERROR(hipMalloc(&Ad, sizeA));
     CHECK_HIP_ERROR(hipMalloc(&Xd, sizeX));
     CHECK_HIP_ERROR(hipMalloc(&Yd, sizeY));
@@ -247,7 +237,7 @@ int main(int argc, char *argv[])
 
     hipSetDevice(1);
 
-    CHECK_ROCBLAS_ERROR(test_hgemv( handle, n1, n2, Ad, lda, Xd, Yd));
+    CHECK_ROCBLAS_ERROR(test_hgemv( handle, n1, n2, Ad, lda, Xd, Yd, alpha, beta));
 
     CHECK_HIP_ERROR(hipMemcpy(Y.data(), Yd, sizeY, hipMemcpyDeviceToHost));
 
@@ -256,11 +246,11 @@ int main(int argc, char *argv[])
     std::cout << "---------------------------------------------" << std::endl;
 
 
-    _Float16 max_error = 0;
+    __fp16 max_error = 0;
     for(int i = 0; i < n1; i++)
     {
-        _Float16 error = (Y[i] - Y_gold[i]) / Y_gold[i];
-        _Float16 abs_error = error >= 0 ? error : -error;
+        __fp16 error = (Y[i] - Y_gold[i]) / Y_gold[i];
+        __fp16 abs_error = error >= 0 ? error : -error;
         max_error = max_error > abs_error ? max_error : abs_error;
 //      std::cout << "error, abs_error, max_error = " << static_cast<float>(error) << ", " << static_cast<float>(abs_error) << ", " << static_cast<float>(max_error) << std::endl;
     }
