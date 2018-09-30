09e0d0fc130e205c6b76bb8f1e7993a7103184bd
amcamd <andrew.chapman@amd.com>
2018-09-30 10:32:28 -0500

    improvements to parse.py

--- Examples/gemm_strided_batched/parse.py
+++ Examples/gemm_strided_batched/parse.py
@@ -14,12 +14,12 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("in_filename",help="input filename")
+    parser.add_argument("dataset",help="dataset name")
     args = parser.parse_args()
-    print "in_filename = ",args.in_filename
 
 #   get filename from command line arguments
-    input_filename = sys.argv[1]
-
+    input_filename = args.in_filename
+    dataset = args.dataset
 
 #   open input file and read from it into lines
     try:
@@ -29,7 +29,6 @@ def main():
         sys.exit()
     lines = f_in.readlines()
 
-
 #   open output file
     f_out=open('./' + input_filename + ".out", 'w+')
 
@@ -37,18 +36,20 @@ def main():
     iterator = 0
     for line in lines:
         iterator = iterator + 1
-        f_out.write("gemm_tuple conv_resnet50_fwd_fp32_%03d=%s" % (iterator,line))
+        f_out.write("gemm_tuple %s_%03d %s" % (dataset,iterator,line))
 
 #   write vector of tuples
     num_lines = file_len(input_filename)
-    f_out.write("\nconst vector<gemm_tuple> deepbench_vec = {\n")
+    f_out.write("\nconst vector<gemm_tuple> %s = {\n" % (dataset))
     for i in range(1,num_lines+1):
-        f_out.write("gemm_tuple conv_resnet50_fwd_fp32_%03d, " % (i))
-        if i%2 == 0:
+        f_out.write("conv_resnet50_fwd_fp32_%03d, " % (i))
+        if i%4 == 0:
             f_out.write("\n")
-    if num_lines%2 != 0:
+    if num_lines%4 != 0:
         f_out.write("\n")
     f_out.write("};\n")
+
+    f_out.write("\nINSTANTIATE_TEST_CASE_P(nightly_%s, parameterized_float_gemm, ValuesIn(%s));" % (dataset, dataset))
 
 
 if __name__ == "__main__":
