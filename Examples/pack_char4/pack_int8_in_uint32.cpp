87c755f143edbf9a52e87a167d962f5d22b6ab50
amcamd <andrew.chapman@amd.com>
2018-10-18 13:36:35 -0500

    improvements to pack_int8_in_uint32

--- Examples/pack_char4/pack_int8_in_uint32.cpp
+++ Examples/pack_char4/pack_int8_in_uint32.cpp
@@ -1,23 +1,59 @@
 #include<iostream>
+#include <cinttypes>
 
-inline int pack4chars(char c1, char c2, char c3, char c4)
+inline int pack4chars(char c_0, char c_1, char c_2, char c_3)
 {
-    return ((int)(((unsigned char)c1) << 24)
-    |  (int)(((unsigned char)c2) << 16)
-    |  (int)(((unsigned char)c3) << 8)
-    |  (int)((unsigned char)c4));
+    return ((int)(((unsigned char)c_0) << 24)
+    |  (int)(((unsigned char)c_1) << 16)
+    |  (int)(((unsigned char)c_2) << 8)
+    |  (int)((unsigned char)c_3));
+}
+
+typedef union{
+    int8_t byte[4];
+    uint32_t uval;
+    int32_t val;
+} int8x4;
+
+void unpack_int8x4(uint32_t in, int32_t &out_0, int32_t &out_1, int32_t &out_2, int32_t &out_3)
+{
+    int8x4 x;
+    x.uval = in;
+    out_0 = x.byte[0];
+    out_1 = x.byte[1];
+    out_2 = x.byte[2];
+    out_3 = x.byte[3];
 }
 
 int main(int argc, char *argv[]) 
 {
-    char c1 = 1;
-    char c2 = 2;
-    char c3 = 3;
-    char c4 = 4;
+    char c_0 = 1;
+    char c_1 = -2;
+    char c_2 = 3;
+    char c_3 = -4;
 
-    uint32_t four_32bit_1 = pack4chars(c1, c2, c3, c4);
+//  uint32_t input = 0x7F8065A3;
+    uint32_t input = pack4chars(c_0, c_1, c_2, c_3);
+    int32_t out_0=0, out_1=0, out_2=0, out_3=0;
+    unpack_int8x4(input, out_0, out_1, out_2, out_3);
+    std::cout << "out_0,1,2,3 = " << out_0 << ", " << out_1 << ", " << out_2 << ", " << out_3 << std::endl;
+
+    uint32_t four_32bit_1 = pack4chars(c_0, c_1, c_2, c_3);
 
     std::cout << "four_32bit_1 hex = " << std::hex << four_32bit_1 << std::endl;
+
+//  int32_t val;
+//  val = 0x7F8065A3;
+
+//  xv = static_cast<signed char *>(&val)[0]; std::cout << " : xv_0 = " << xv;
+//  xv = static_cast<signed char *>(&val)[1]; std::cout << " : xv_1 = " << xv;
+//  xv = static_cast<signed char *>(&val)[2]; std::cout << " : xv_2 = " << xv;
+//  xv = static_cast<signed char *>(&val)[3]; std::cout << " : xv_3 = " << xv;
+//  std::cout << std::endl;
+
+
+
+
 
     char     *A_quad = reinterpret_cast<char    *>(&four_32bit_1);
 
@@ -38,12 +74,12 @@ int main(int argc, char *argv[])
 
     std::cout << "    A_0, A_1, A_2, A_3 = " << A_0 << ", " << A_1 << ", " << A_2 << ", " << A_3 << std::endl;
 
-    c1 = -1;
-    c2 = -2;
-    c3 = -3;
-    c4 = -4;
+    c_0 = -4;
+    c_1 = -1;
+    c_2 = -2;
+    c_3 = -3;
 
-    four_32bit_1 = pack4chars(c1, c2, c3, c4);
+    four_32bit_1 = pack4chars(c_0, c_1, c_2, c_3);
 
     std::cout << "four_32bit_1 hex = " << std::hex << four_32bit_1 << std::endl;
 
