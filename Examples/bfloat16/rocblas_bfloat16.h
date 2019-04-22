e738bfefbaa40be466d7f792827b4c4ed19d2207
amcamd <andrew.chapman@amd.com>
2019-04-22 14:10:08 -0500

    improve formatting

--- Examples/bfloat16/rocblas_bfloat16.h
+++ Examples/bfloat16/rocblas_bfloat16.h
@@ -12,7 +12,6 @@ typedef struct
     uint16_t data;
 } rocblas_bfloat16;
 
-
 // zero extend lower 16 bits of bfloat16 to convert to IEEE float
 float bfloat16_to_float(const rocblas_bfloat16 v)
 {
@@ -52,12 +51,14 @@ inline rocblas_bfloat16 operator+(rocblas_bfloat16 a, rocblas_bfloat16 b) { retu
 inline rocblas_bfloat16 operator-(rocblas_bfloat16 a, rocblas_bfloat16 b) { return float_to_bfloat16(bfloat16_to_float(a) - bfloat16_to_float(b)); }
 inline rocblas_bfloat16 operator*(rocblas_bfloat16 a, rocblas_bfloat16 b) { return float_to_bfloat16(bfloat16_to_float(a) * bfloat16_to_float(b)); }
 inline rocblas_bfloat16 operator/(rocblas_bfloat16 a, rocblas_bfloat16 b) { return float_to_bfloat16(bfloat16_to_float(a) / bfloat16_to_float(b)); }
+
 inline bool operator<(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) < bfloat16_to_float(b); }
 inline bool operator<=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) <= bfloat16_to_float(b); }
 inline bool operator==(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) == bfloat16_to_float(b); }
 inline bool operator!=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) != bfloat16_to_float(b); }
 inline bool operator>(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) > bfloat16_to_float(b); }
 inline bool operator>=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) >= bfloat16_to_float(b); }
+
 inline rocblas_bfloat16& operator+=(rocblas_bfloat16& a, rocblas_bfloat16 b) { a = a + b; return a; }
 inline rocblas_bfloat16& operator-=(rocblas_bfloat16& a, rocblas_bfloat16 b) { a = a - b; return a; }
 inline rocblas_bfloat16& operator*=(rocblas_bfloat16& a, rocblas_bfloat16 b) { a = a * b; return a; }
@@ -66,6 +67,7 @@ inline rocblas_bfloat16& operator/=(rocblas_bfloat16& a, rocblas_bfloat16 b) { a
 inline bool isinf(const rocblas_bfloat16& a) { return std::isinf(bfloat16_to_float(a)); }
 inline bool isnan(const rocblas_bfloat16& a) { return std::isnan(bfloat16_to_float(a)); }
 inline bool iszero(const rocblas_bfloat16& a) { return (a.data & 0x7FFF) == 0; }
+
 inline rocblas_bfloat16 abs(const rocblas_bfloat16& a) { return float_to_bfloat16(std::abs(bfloat16_to_float(a))); }
 inline rocblas_bfloat16 sin(const rocblas_bfloat16& a) { return float_to_bfloat16(std::sin(bfloat16_to_float(a))); }
 inline rocblas_bfloat16 cos(const rocblas_bfloat16& a) { return float_to_bfloat16(std::cos(bfloat16_to_float(a))); }
