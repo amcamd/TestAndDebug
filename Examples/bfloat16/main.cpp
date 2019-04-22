943a733b5f2326f6c0c81d9ce65195c78f7d3087
amcamd <andrew.chapman@amd.com>
2019-04-22 14:08:23 -0500

    simplify struct rocblas_bfloat16

--- Examples/bfloat16/main.cpp
+++ Examples/bfloat16/main.cpp
@@ -11,29 +11,64 @@
 int main()
 {
 //  test multiply add
-    rocblas_bfloat16 b0 = rocblas_bfloat16(0.0f); float f0 = 0.0f;
-    rocblas_bfloat16 b1 = rocblas_bfloat16(1.0f); float f1 = 1.0f;
-    rocblas_bfloat16 b2 = rocblas_bfloat16(2.0f); float f2 = 2.0f;
-    rocblas_bfloat16 b3 = rocblas_bfloat16(3.0f); float f3 = 3.0f;
-    rocblas_bfloat16 b4 = rocblas_bfloat16(4.0f); float f4 = 4.0f;
-    rocblas_bfloat16 b5 = rocblas_bfloat16(5.0f); float f5 = 5.0f;
-    rocblas_bfloat16 b6 = rocblas_bfloat16(6.0f); float f6 = 6.0f;
-    rocblas_bfloat16 b7 = rocblas_bfloat16(7.0f); float f7 = 7.0f;
-    rocblas_bfloat16 b8 = rocblas_bfloat16(8.0f); float f8 = 8.0f;
-    rocblas_bfloat16 b9 = rocblas_bfloat16(9.0f); float f9 = 9.0f;
-    rocblas_bfloat16 b1_1 = rocblas_bfloat16(1.1f); float f1_1 = 1.1f;
+    rocblas_bfloat16 b0 = float_to_bfloat16(0.0f); float f0 = 0.0f;
+    rocblas_bfloat16 b1 = float_to_bfloat16(1.0f); float f1 = 1.0f;
+    rocblas_bfloat16 b2 = float_to_bfloat16(2.0f); float f2 = 2.0f;
+    rocblas_bfloat16 b3 = float_to_bfloat16(3.0f); float f3 = 3.0f;
+    rocblas_bfloat16 b4 = float_to_bfloat16(4.0f); float f4 = 4.0f;
+    rocblas_bfloat16 b5 = float_to_bfloat16(5.0f); float f5 = 5.0f;
+    rocblas_bfloat16 b6 = float_to_bfloat16(6.0f); float f6 = 6.0f;
+    rocblas_bfloat16 b7 = float_to_bfloat16(7.0f); float f7 = 7.0f;
+    rocblas_bfloat16 b8 = float_to_bfloat16(8.0f); float f8 = 8.0f;
+    rocblas_bfloat16 b9 = float_to_bfloat16(9.0f); float f9 = 9.0f;
+    rocblas_bfloat16 b1_1 = float_to_bfloat16(1.1f); float f1_1 = 1.1f;
 
-    rocblas_bfloat16 bb = b2 * ((b3 * b4) + (b5 * b6)) + b7 * b8;
-    float            ff = f2 * ((f3 * f4) + (f5 * f6)) + f7 * f8;
+    rocblas_bfloat16 bb = b2 * ((b3 * b4) + (b5 * b6)) - b7 * b8;
+    float            ff = f2 * ((f3 * f4) + (f5 * f6)) - f7 * f8;
 
-    std::cout << "calculated bb = a2 * ((a3 * b4) + (a5 * b6)) + a7 * a8 = " << bb << std::endl;
-    std::cout << "reference  ff = f2 * ((f3 * f4) + (f5 * f6)) + f7 * f8 = " << ff << std::endl << std::endl;
+    std::cout << "test operators *, +, - " << std::endl;
+    std::cout << "calculated bb = b2 * ((b3 * b4) + (b5 * b6)) - b7 * b8 = " << bb << std::endl;
+    std::cout << "reference  ff = f2 * ((f3 * f4) + (f5 * f6)) - f7 * f8 = " << ff << std::endl << std::endl;
 
-    bb = b1_1 * ((b3 * b4) + (b5 * b6)) + b7 * b8;
-    ff = f1_1 * ((f3 * f4) + (f5 * f6)) + f7 * f8;
+    std::cout << "calculated b1_1 * b3 = " << b1_1 * b3 << std::endl;
+    std::cout << "reference  f1_1 * f3 = " << f1_1 * f3 << std::endl;
 
-    std::cout << "calculated bb = a1_1 * ((a3 * b4) + (a5 * b6)) + a7 * a8 = " << bb << std::endl;
-    std::cout << "reference  ff = f1_1 * ((f3 * f4) + (f5 * f6)) + f7 * f8 = " << ff << std::endl;
+    std::cout << "test operator / " << std::endl;
+    std::cout << "calculated b8 / b2 = " << b8 / b2 << std::endl;
+    std::cout << "reference  f8 / f2 = " << f8 / f2 << std::endl;
+
+    std::cout << "test operators: >, <, ==, !=, <=, >=" << std::endl;
+    b2 > b1 ? std::cout << "PASS: b2 > b1\n" : std::cout << "FAIL: b2 > b1\n";
+    b1 < b2 ? std::cout << "PASS: b1 < b2\n" : std::cout << "FAIL: b1 > b2\n";
+
+    b1 == b1 ? std::cout << "PASS: b1 == b2\n" : std::cout << "FAIL: b1 == b2\n";
+    b1 == b2 ? std::cout << "FAIL: b1 == b2\n" : std::cout << "PASS: b1 == b2\n";
+
+    b1 != b1 ? std::cout << "FAIL: b1 != b1\n" : std::cout << "PASS: b1 != b1\n";
+    b1 != b2 ? std::cout << "PASS: b1 != b2\n" : std::cout << "FAIL: b1 != b2\n";
+
+    b1 >= b1 ? std::cout << "PASS: b1 >= b1\n" : std::cout << "FAIL: b1 >= b1\n";
+    b2 >= b1 ? std::cout << "PASS: b2 >= b1\n" : std::cout << "FAIL: b2 >= b1\n";
+    b1 >= b2 ? std::cout << "FAIL: b1 >= b2\n" : std::cout << "PASS: b1 >= b2\n"; 
+
+    b1 <= b1 ? std::cout << "PASS: b1 <= b1\n" : std::cout << "FAIL: b1 <= b1\n";
+    b2 <= b1 ? std::cout << "FAIL: b2 <= b1\n" : std::cout << "PASS: b2 <= b1\n";
+    b1 <= b2 ? std::cout << "PASS: b1 <= b2\n" : std::cout << "FAIL: b1 <= b2\n";
+
+    rocblas_bfloat16 tt;
+    tt = b2; (tt += b1) == b3 ? std::cout << "PASS: (b2 += b1) == b3\n" : std::cout << "FAIL: (b2 += b1) == b3\n";
+    tt = b2; (tt -= b1) == b1 ? std::cout << "PASS: (b2 -= b1) == b1\n" : std::cout << "FAIL: (b2 -= b1) == b1\n";
+    tt = b2; (tt *= b3) == b6 ? std::cout << "PASS: (b2 *= b3) == b6\n" : std::cout << "FAIL: (b2 *= b3) == b6\n";
+    tt = b6; (tt /= b2) == b3 ? std::cout << "PASS: (b6 /= b2) == b3\n" : std::cout << "FAIL: (b6 /= b2) == b3\n";
+
+    tt = b2 / b0; if(isinf(tt)) {std::cout << "PASS: isinf\n";} else { std::cout << "FAIL: isinf\n";};
+    tt = tt / tt; if(isnan(tt)) {std::cout << "PASS: isnan\n";} else { std::cout << "FAIL: isnan\n";};
+
+    b8 == abs(b9 - b1) ? std::cout << "PASS: abs\n" : std::cout << "FAIL: abs\n";
+    b9 == abs(b7 + b1) ? std::cout << "FAIL: abs\n" : std::cout << "PASS: abs\n";
+
+    sin(b0) == b0 ? std::cout << "PASS: sin(b0) == 0\n" : std::cout << "FAIL: sin(b0) == 0\n";
+    cos(b0) == b1 ? std::cout << "PASS: cos(b0) == b1\n" : std::cout << "FAIL: cos(b0) == b1\n";
 
     return 0;
 }
--- Examples/bfloat16/rocblas_bfloat16.h
+++ Examples/bfloat16/rocblas_bfloat16.h
@@ -5,86 +5,67 @@
 #define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
 #endif
 
-typedef struct rocblas_bfloat16
+#define BFLOAT16_Q_NAN_VALUE 0xFFC1
+
+typedef struct
 {
-    rocblas_bfloat16() : data(0){}
-
-    // truncate lower 16 bits of IEEE float to convert to bfloat16
-    static rocblas_bfloat16 float_to_bfloat16(const float v) 
-    {
-        rocblas_bfloat16 bf16;
-        if (std::isnan(v))
-        {
-            bf16.data = Q_NAN_VALUE;
-            return bf16;
-        }
-        const uint16_t* p_ui16= reinterpret_cast<const uint16_t*>(&v);
-#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-        bf16.data = p_ui16[0];
-#else
-        bf16.data = p_ui16[1];
-#endif
-        return bf16;
-    }
-
-    explicit rocblas_bfloat16(const float v) 
-    {
-        data = float_to_bfloat16(v).data;
-    }
-
-    // zero extend lower 16 bits of bfloat16 to convert to IEEE float
-    explicit operator float() const
-    {
-        float fp32 = 0;
-
-        uint16_t* q = reinterpret_cast<uint16_t*>(&fp32);
-
-#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-        q[0] = data;
-#else
-        q[1] = data;
-#endif
-        return fp32;
-    }
-
-    bool is_zero() const 
-    { 
-        return (data & 0x7FFF) == ZERO_VALUE; 
-    }
-
     uint16_t data;
-
-    private:
-
-    static const uint16_t Q_NAN_VALUE = 0xFFC1;
-
-    static const uint16_t ZERO_VALUE = 0;
-
 } rocblas_bfloat16;
 
-inline std::ostream& operator<<(std::ostream& os, const rocblas_bfloat16& bf16) { os << static_cast<float>(bf16); return os; }
 
-inline rocblas_bfloat16 operator+(rocblas_bfloat16 a, rocblas_bfloat16 b) { return rocblas_bfloat16(static_cast<float>(a) + static_cast<float>(b)); }
-inline rocblas_bfloat16 operator-(rocblas_bfloat16 a, rocblas_bfloat16 b) { return rocblas_bfloat16(static_cast<float>(a) - static_cast<float>(b)); }
-inline rocblas_bfloat16 operator*(rocblas_bfloat16 a, rocblas_bfloat16 b) { return rocblas_bfloat16(static_cast<float>(a) * static_cast<float>(b)); }
-inline rocblas_bfloat16 operator/(rocblas_bfloat16 a, rocblas_bfloat16 b) { return rocblas_bfloat16(static_cast<float>(a) / static_cast<float>(b)); }
-inline bool operator<(rocblas_bfloat16 a, rocblas_bfloat16 b) { return static_cast<float>(a) < static_cast<float>(b); }
-inline bool operator<=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return static_cast<float>(a) <= static_cast<float>(b); }
-inline bool operator==(rocblas_bfloat16 a, rocblas_bfloat16 b) { return static_cast<float>(a) == static_cast<float>(b); }
-inline bool operator!=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return static_cast<float>(a) != static_cast<float>(b); }
-inline bool operator>(rocblas_bfloat16 a, rocblas_bfloat16 b) { return static_cast<float>(a) > static_cast<float>(b); }
-inline bool operator>=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return static_cast<float>(a) >= static_cast<float>(b); }
+// zero extend lower 16 bits of bfloat16 to convert to IEEE float
+float bfloat16_to_float(const rocblas_bfloat16 v)
+{
+    float fp32 = 0;
+
+    uint16_t* q = reinterpret_cast<uint16_t*>(&fp32);
+
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    q[0] = v.data;
+#else
+    q[1] = v.data;
+#endif
+    return fp32;
+}
+
+// truncate lower 16 bits of IEEE float to convert to bfloat16
+rocblas_bfloat16 float_to_bfloat16(const float v) 
+{
+    rocblas_bfloat16 bf16;
+    if (std::isnan(v))
+    {
+        bf16.data = BFLOAT16_Q_NAN_VALUE;
+        return bf16;
+    }
+    const uint16_t* p_ui16= reinterpret_cast<const uint16_t*>(&v);
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    bf16.data = p_ui16[0];
+#else
+    bf16.data = p_ui16[1];
+#endif
+    return bf16;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const rocblas_bfloat16& bf16) { os << bfloat16_to_float(bf16); return os; }
+
+inline rocblas_bfloat16 operator+(rocblas_bfloat16 a, rocblas_bfloat16 b) { return float_to_bfloat16(bfloat16_to_float(a) + bfloat16_to_float(b)); }
+inline rocblas_bfloat16 operator-(rocblas_bfloat16 a, rocblas_bfloat16 b) { return float_to_bfloat16(bfloat16_to_float(a) - bfloat16_to_float(b)); }
+inline rocblas_bfloat16 operator*(rocblas_bfloat16 a, rocblas_bfloat16 b) { return float_to_bfloat16(bfloat16_to_float(a) * bfloat16_to_float(b)); }
+inline rocblas_bfloat16 operator/(rocblas_bfloat16 a, rocblas_bfloat16 b) { return float_to_bfloat16(bfloat16_to_float(a) / bfloat16_to_float(b)); }
+inline bool operator<(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) < bfloat16_to_float(b); }
+inline bool operator<=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) <= bfloat16_to_float(b); }
+inline bool operator==(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) == bfloat16_to_float(b); }
+inline bool operator!=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) != bfloat16_to_float(b); }
+inline bool operator>(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) > bfloat16_to_float(b); }
+inline bool operator>=(rocblas_bfloat16 a, rocblas_bfloat16 b) { return bfloat16_to_float(a) >= bfloat16_to_float(b); }
 inline rocblas_bfloat16& operator+=(rocblas_bfloat16& a, rocblas_bfloat16 b) { a = a + b; return a; }
 inline rocblas_bfloat16& operator-=(rocblas_bfloat16& a, rocblas_bfloat16 b) { a = a - b; return a; }
-inline rocblas_bfloat16 operator++(rocblas_bfloat16& a) { a += rocblas_bfloat16(1); return a; }
-inline rocblas_bfloat16 operator--(rocblas_bfloat16& a) { a -= rocblas_bfloat16(1); return a; }
-inline rocblas_bfloat16 operator++(rocblas_bfloat16& a, int) { rocblas_bfloat16 original_value = a; ++a; return original_value; }
-inline rocblas_bfloat16 operator--(rocblas_bfloat16& a, int) { rocblas_bfloat16 original_value = a; --a; return original_value; }
 inline rocblas_bfloat16& operator*=(rocblas_bfloat16& a, rocblas_bfloat16 b) { a = a * b; return a; }
 inline rocblas_bfloat16& operator/=(rocblas_bfloat16& a, rocblas_bfloat16 b) { a = a / b; return a; }
 
-inline bool isinf(const rocblas_bfloat16& a) { return std::isinf(float(a)); }
-inline bool isnan(const rocblas_bfloat16& a) { return std::isnan(float(a)); }
-inline rocblas_bfloat16 abs(const rocblas_bfloat16& a) { return rocblas_bfloat16(std::abs(float(a))); }
-inline rocblas_bfloat16 sin(const rocblas_bfloat16& a) { return rocblas_bfloat16(std::sin(float(a))); }
-inline rocblas_bfloat16 cos(const rocblas_bfloat16& a) { return rocblas_bfloat16(std::cos(float(a))); }
+inline bool isinf(const rocblas_bfloat16& a) { return std::isinf(bfloat16_to_float(a)); }
+inline bool isnan(const rocblas_bfloat16& a) { return std::isnan(bfloat16_to_float(a)); }
+inline bool iszero(const rocblas_bfloat16& a) { return (a.data & 0x7FFF) == 0; }
+inline rocblas_bfloat16 abs(const rocblas_bfloat16& a) { return float_to_bfloat16(std::abs(bfloat16_to_float(a))); }
+inline rocblas_bfloat16 sin(const rocblas_bfloat16& a) { return float_to_bfloat16(std::sin(bfloat16_to_float(a))); }
+inline rocblas_bfloat16 cos(const rocblas_bfloat16& a) { return float_to_bfloat16(std::cos(bfloat16_to_float(a))); }
