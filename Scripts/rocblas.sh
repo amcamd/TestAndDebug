abe97cfedface3dcbe389c2a882d9bbba103db19
amcamd <andrew.chapman@amd.com>
2019-08-21 12:02:00 -0500

    add script to build and run rocBLAS

---
+++ Scripts/rocblas.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -x
+
+echo "==============================================================="
+echo "=====clone=rocblas============================================="
+echo "==============================================================="
+git clone -b develop https://github.com/amcamd/rocBLAS.git 
+if [[ $? -ne 0 ]]; then
+    echo "clone error"
+    exit 1
+fi
+
+cd rocBLAS
+if [[ $? -ne 0 ]]; then
+    echo "cd error"
+    exit 1
+fi
+
+echo "==============================================================="
+echo "=====build=rocblas=with=install.sh============================="
+echo "==============================================================="
+./install.sh -lasm_ci -c 2>&1 | tee install.out
+if [[ $? -ne 0 ]]; then
+    echo "install error"
+    exit 1
+fi
+
+cd build/release/clients/staging
+if [[ $? -ne 0 ]]; then
+    echo "cd staging error"
+    exit 1
+fi
+
+echo "==============================================================="
+echo "=====run=quick=tests==========================================="
+echo "==============================================================="
+LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./rocblas-test --gtest_filter=*quick* 
+if [[ $? -ne 0 ]]; then
+    echo "quick test error"
+    exit 1
+fi
+
+
+echo "==============================================================="
+echo "=====run=pre_checkin=tests====================================="
+echo "==============================================================="
+LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./rocblas-test --gtest_filter=*pre_checkin*
+if [[ $? -ne 0 ]]; then
+    echo "pre_checkin test error"
+    exit 1
+fi
+
+
+echo "==============================================================="
+echo "=====run=nightly=tests========================================="
+echo "==============================================================="
+LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./rocblas-test --gtest_filter=*nightly* 
+if [[ $? -ne 0 ]]; then
+    echo "nightly test error"
+    exit 1
+fi
+
