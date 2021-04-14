2c6892d76fd47a0caed8ad6334c7900d6367cfe6
amcamd <andrew.chapman@amd.com>
2021-04-14 19:26:08 -0400

    update rocblas.sh

--- Scripts/rocblas.sh
+++ Scripts/rocblas.sh
@@ -6,6 +6,11 @@
 #sudo dpkg -r rocblas
 #export PATH=/opt/rocm/bin:$PATH
 
+# docker commands
+#sudo docker run --name andrew -tid --ipc=host --cap-add=SYS_PTRACE --privileged --security-opt seccomp=unconfined --device=/dev/kfd --device=/dev/dri --group-add video --group-add 44 -v /home:/home compute-artifactory.amd.com:5000/rocm-plus-docker/compute-rocm-rel-4.2:10-STG1 /bin/bash || exit 1
+#
+#sudo docker exec -e COLUMNS="`tput cols`" -e LINES="`tput lines`" -ti b3285621ba95 /bin/bash
+
 echo "=====clone=rocblas============================================="
 echo "==============================================================="
 #git clone https://github.com/amcamd/rocBLAS.git 
@@ -37,11 +42,23 @@ if [ "$(/opt/rocm/bin/rocm_agent_enumerator | grep -m 1 gfx900)" == "gfx900" ]; 
     echo "=====ISA = gfx900, use -agfx900 directive ===================="
     time VERBOSE=1 ./install.sh -agfx900 -c 2>&1 | tee install.out
 elif [ "$(/opt/rocm/bin/rocm_agent_enumerator | grep -m 1 gfx906)" == "gfx906" ]; then
-    echo "=====ISA = gfx906, use -agfx906 directive ===================="
-    time VERBOSE=1 ./install.sh -agfx906 -c 2>&1 | tee install.out
+    echo "=====ISA = gfx906, use -agfx906:xnack- directive ===================="
+    time VERBOSE=1 ./install.sh -agfx906:xnack- -c 2>&1 | tee install.out
 elif [ "$(/opt/rocm/bin/rocm_agent_enumerator | grep -m 1 gfx908)" == "gfx908" ]; then
-    echo "=====ISA = gfx908, use -agfx908 directive ===================="
-    time VERBOSE=1 ./install.sh -agfx908 -c 2>&1 | tee install.out
+    echo "=====ISA = gfx908, use -agfx908:xnack- directive ===================="
+    time VERBOSE=1 ./install.sh -agfx908:xnack- -c 2>&1 | tee install.out
+elif [ "$(/opt/rocm/bin/rocm_agent_enumerator | grep -m 1 gfx90a)" == "gfx908" ]; then
+    echo "=====ISA = gfx908, use -agfx908:xnack- directive ===================="
+    time VERBOSE=1 ./install.sh -agfx90a:xnack- -c 2>&1 | tee install.out
+elif [ "$(/opt/rocm/bin/rocm_agent_enumerator | grep -m 1 gfx1010)" == "gfx908" ]; then
+    echo "=====ISA = gfx908, use -agfx908:xnack- directive ===================="
+    time VERBOSE=1 ./install.sh -agfx1010 -c 2>&1 | tee install.out
+elif [ "$(/opt/rocm/bin/rocm_agent_enumerator | grep -m 1 gfx1012)" == "gfx908" ]; then
+    echo "=====ISA = gfx908, use -agfx908:xnack- directive ===================="
+    time VERBOSE=1 ./install.sh -agfx1012 -c 2>&1 | tee install.out
+elif [ "$(/opt/rocm/bin/rocm_agent_enumerator | grep -m 1 gfx1030)" == "gfx908" ]; then
+    echo "=====ISA = gfx908, use -agfx908:xnack- directive ===================="
+    time VERBOSE=1 ./install.sh -agfx1030 -c 2>&1 | tee install.out
 else
     echo "build fat binary, ISA != gfx900 and ISA != gfx906 and ISA != gfx908"
     time VERBOSE=1 ./install.sh -c 2>&1 | tee install.out
