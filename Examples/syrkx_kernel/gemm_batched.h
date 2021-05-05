deca56ae9846786f987fdea8bef147f188f5186d
amcamd <andrew.chapman@amd.com>
2021-05-05 13:26:03 -0500

    add syrkx_kernel

---
+++ Examples/syrkx_kernel/gemm_batched.h
@@ -0,0 +1,16 @@
+    template <typename T>
+    void gemm_batched_solution(rocblas_operation trans_a,
+                               rocblas_operation trans_b,
+                               rocblas_int       m,
+                               rocblas_int       n,
+                               rocblas_int       k,
+                               const T           alpha,
+                               const T* const    dA_array[],
+                               rocblas_int       lda,
+                               const T* const    dB_array[],
+                               rocblas_int       ldb,
+                               const T           beta,
+                               T* const          dC_array[],
+                               rocblas_int       ldc,
+                               rocblas_int       batch_count,
+                               hipStream_t       stream);
