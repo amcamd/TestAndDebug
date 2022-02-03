f09e0d05a7f8c2d383f73877fb6818fc7b7fb281
amcamd <andrew.chapman@amd.com>
2022-02-03 10:06:27 -0600

    generalize update_branch to hipBLAS

--- Scripts/update_branch.sh
+++ Scripts/update_branch.sh
@@ -6,24 +6,64 @@ UPSTREAM="ROCmSoftwarePlatform"
 
 #read command line arguments
 #TODO add ORIGIN and UPSTREAM forks
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
 
-case $key in
+BRANCH=develop
+REPOSITORY=rocBLAS-internal
+
+echo "BRANCH = $BRANCH"
+echo "REPOSITORY = $REPOSITORY"
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
     -b|--branch)
-    BRANCH="$2"
-    shift
-    shift
-    ;;
+      BRANCH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -r|--repository)
+      REPOSITORY="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
     *)
-    echo "Usage: $0 -b <branch>"
-    echo "where branch =  master  or  staging  or   develop"
-    exit 1
-    ;;
-esac
+      echo "unknown argument"
+      echo "Usage: $0 -b <branch> -r <repository>"
+      echo "where branch =  master  or  staging  or   develop"
+      echo "where repository == rocBLAS-internal, rocBLAS, hipBLAS"
+      exit 1
+      ;;
+  esac
 done
+
+
+echo "BRANCH = $BRANCH"
+echo "REPOSITORY = $REPOSITORY"
+
+
+#POSITIONAL=()
+#while [[ $# -gt 0 ]]
+#do
+#key="$1"
+#
+#case $key in
+#    -b|--branch)
+#    BRANCH="$2"
+#    shift
+#    shift
+#    ;;
+#    *)
+#    echo "Usage: $0 -b <branch>"
+#    echo "where branch =  master  or  staging  or   develop"
+#    exit 1
+#    ;;
+#esac
+#done
 
 #require branch to be one of: master, staging, develop
 if [[ $BRANCH != "master" ]] && [[ $BRANCH != "staging" ]] && [[ $BRANCH != "develop" ]]; then
@@ -32,9 +72,16 @@ if [[ $BRANCH != "master" ]] && [[ $BRANCH != "staging" ]] && [[ $BRANCH != "dev
     exit 1
 fi
 
+#require repository to be one of: rocBLAS-internal, rocBLAS, hipBLAS
+if [[ $REPOSITORY != "rocBLAS-internal" ]] && [[ $REPOSITORY != "rocBLAS" ]] && [[ $REPOSITORY != "hipBLAS" ]]; then
+    echo "Usage: $0 -r <repository>"
+    echo "where repository =  rocBLAS-internal  or  rocBLAS  or  hipBLAS"
+    exit 1
+fi
+
 #prompt for confirmation to proceed
 while true; do
-    read -p "Confirm you want to update $ORIGIN/$BRANCH?" yn
+    read -p "Confirm you want to update $ORIGIN:$REPOSITORY/$BRANCH?" yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) echo "exiting script"; exit;;
@@ -45,13 +92,13 @@ done
 #clone branch and rebase it on upstream/$BRANCH
 mkdir -p ~/repos/rocBLASmergeDevelop
 cd ~/repos/rocBLASmergeDevelop
-rm -rf rocBLAS-internal
+rm -rf $REPOSITORY
 
-git clone  git@github.com:$ORIGIN/rocBLAS-internal.git
-cd rocBLAS-internal
+git clone  git@github.com:$ORIGIN/$REPOSITORY.git
+cd $REPOSITORY
 git checkout $BRANCH
 
-git remote add upstream git@github.com:$UPSTREAM/rocBLAS-internal.git
+git remote add upstream git@github.com:$UPSTREAM/$REPOSITORY.git
 git fetch upstream
 
 git rebase upstream/$BRANCH $BRANCH
@@ -59,10 +106,10 @@ git status
 
 #prompt for confirmation before pushing to origin/$BRANCH
 while true; do
-    read -p "Do you wish to push to origin/$BRANCH?" yn
+    read -p "Do you wish to push to origin:$REPOSITORY/$BRANCH?" yn
     case $yn in
         [Yy]* ) git push; break;;
-        [Nn]* ) echo "change is in ~/repos/rocBLASmergeDevelop/rocBLAS-internal"; exit;;
+        [Nn]* ) echo "change is in ~/repos/rocBLASmergeDevelop/$REPOSITORY"; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
