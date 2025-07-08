792b0f7000dea62df7896e6e1694ead69b406e27
Andrew Chapman <anchapman@ctr2-alola-login-02.amd.com>
2025-07-08 15:54:00 -0400

    update clone_rocblas.sh for mono-repo

--- Scripts/clone_rocblas.sh
+++ Scripts/clone_rocblas.sh
@@ -3,21 +3,21 @@
 function display_help()
 {
 cat <<EOF
-Script to clone rocBLAS-internal, rocBLAS, or hipBLAS
+Script to clone mono-repo, rocBLAS-internal, rocBLAS, or hipBLAS
 
   Usage:
     $0 <options>
 
   Options:
-    -r|--repository     rocBLAS_internal, rocBLAS, hipBLAS      Default rocBLAS-internal)
+    -r|--repository     mono-repo, rocBLAS_internal, rocBLAS, hipBLAS      Default mono-repo)
     -o|--origin         amcamd or ROCm    Default amcamd)
     -c|--connection     ssh or https                      Default ssh
     -h|--help           this help information)
 EOF
 }
 
-ORIGIN="amcamd"
-REPOSITORY="rocBLAS-internal"
+ORIGIN="ROCm"
+REPOSITORY="mono-repo"
 CONNECTION="ssh"
 
 while [[ $# -gt 0 ]]; do
@@ -49,16 +49,16 @@ while [[ $# -gt 0 ]]; do
       echo "unknown argument"
       echo "Usage: $0 -b <branch> -r <repository>"
       echo "where branch =  master  or  staging  or   develop"
-      echo "where repository == rocBLAS-internal, rocBLAS, hipBLAS"
+      echo "where repository == mono-repo, rocBLAS-internal, rocBLAS, hipBLAS"
       exit 1
       ;;
   esac
 done
 
-#require repository to be one of: rocBLAS-internal, rocBLAS, hipBLAS
-if [[ $REPOSITORY != "rocBLAS-internal" ]] && [[ $REPOSITORY != "rocBLAS" ]] && [[ $REPOSITORY != "hipBLAS" ]]; then
+#require repository to be one of: mono-reop, rocBLAS-internal, rocBLAS, hipBLAS
+if [[ $REPOSITORY != "mono-repo" ]] && [[ $REPOSITORY != "rocBLAS-internal" ]] && [[ $REPOSITORY != "rocBLAS" ]] && [[ $REPOSITORY != "hipBLAS" ]]; then
     echo "Usage: $0 -r <repository>"
-    echo "where repository =  rocBLAS-internal, rocBLAS, or hipBLAS"
+    echo "where repository =  mono-repo, rocBLAS-internal, rocBLAS, or hipBLAS"
     exit 1
 fi
 
@@ -76,7 +76,16 @@ if [[ $CONNECTION == "https" ]]; then
     cat ~/githubToken
     git clone https://github.com/$ORIGIN/$REPOSITORY.git
 else
-    git clone  git@github.com:$ORIGIN/$REPOSITORY.git
+
+    if [[ $ORIGIN == "ROCm" && [[ $REPOSITORY == "mono-repo" ]]; then
+        git clone --no-checkout --filter=blob:none https://github.com/ROCm/rocm-libraries.git
+        cd rocm-libraries
+        git sparse-checkout init --cone
+        git sparse-checkout set projects/rocblas shared/tensile
+        git checkout develop
+    else
+        git clone  git@github.com:$ORIGIN/$REPOSITORY.git
+    fi
 fi
 
 if [[ $? -ne 0 ]]; then
